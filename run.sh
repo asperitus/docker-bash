3ed43b9fe508aa6854e4cc86b16d610ed48b937f
Qiang Li <qiang4.li@ge.com>
2017-12-27 19:17:39 -0800

    first commit

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#
+set -x
+
+#docker pull dhnt/bash
+
+# behind corporate firewall - http_proxy host needs to be ip address
+# e.g. export http_proxy=http://10.10.10.10:8080
+
+[ -z "$http_proxy" ] && proxy="" || proxy="-e http_proxy=$http_proxy -e https_proxy=$http_proxy -e no_proxy=$no_proxy"
+
+#
+export DHNT_BASE=~/.dhnt
+export DISPLAY=192.168.56.1:0
+
+docker run --detach $proxy -v ${DHNT_BASE}:/home/vcap -e DISPLAY=${DISPLAY} -it --rm --privileged --name dhnt-bash-$$ dhnt/bash
---
+++ script/install-pkg.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -x
+
+##
+echo "Install additional dependencies ..."
+
+#sudo apt-get install -y --no-install-recommends $pkg
+
+##
+echo "Install additional packages ..."
+
+exit 0
